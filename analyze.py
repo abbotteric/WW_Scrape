d09b72937c810e37904e7d210a6393eb7562993c
Eric Abbott <ericabbott1@gmail.com>
2012-03-05 17:04:56 -0800

    Finished at DiCon 3-5-12

--- analyze.py
+++ analyze.py
@@ -1,5 +1,14 @@
 import sys
 import numpy as np
+import re
+
+f = open('PPlus.csv','r+')
+f.readline()
+line = f.readline()
+while (line):
+	m = re.search('(.*),(.*),(.*),(.*),([0-9]*)',line)
+	print m.group(3)
+	line = f.readline()
 
 x = np.array([[1,2,3],[4,5,6]],np.int32)
 print type(x)
